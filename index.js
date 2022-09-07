dd2a70d76e24daa4c87089e220e67800175144d2
isaif <saif935775@gmail.com>
2022-09-07 15:13:24 +0530

    feat(navigation): ctrl-h and ctrl-l to navigate tabs

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ import theme from "./gruvbox-simple.js";
 settings.theme = theme;
 
 api.aceVimMap("jj", "<Esc>", "insert");
+
+// tab navigation
+api.map("<Ctrl-h>", "E");
+api.unmap("E");
+api.map("<Ctrl-l>", "R");
+api.unmap("R");
